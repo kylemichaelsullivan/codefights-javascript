2e840b723f976602d1de42c5d4896778e8c5b4f9
Kyle Michael Sullivan <kylemichaelsullivan@gmail.com>
2018-06-15 01:34:22 -0400

    Update 43-isBeautifulString.js

--- Intro/10 Eruption of Light/43-isBeautifulString.js
+++ Intro/10 Eruption of Light/43-isBeautifulString.js
@@ -9,7 +9,7 @@ function isBeautifulString(inputString) {
     }
 
     for(let i=1;i<array.length;i++){
-        if(array[i]>array[i-1]) return false;
+        if(array[i]>array[i-1])return false;
     }
     return true;
 }
