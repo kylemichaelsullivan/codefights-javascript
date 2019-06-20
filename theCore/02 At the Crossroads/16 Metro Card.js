f207b5295070a3f1da5b80055af4e310bb3bdbf1
Kyle Michael Sullivan <kylemichaelsullivan@gmail.com>
2019-06-19 22:27:58 -0400

    Significantly simplified 16 Metro Card

--- theCore/02 At the Crossroads/16 Metro Card.js
+++ theCore/02 At the Crossroads/16 Metro Card.js
@@ -1,24 +1,5 @@
 // https://app.codesignal.com/arcade/code-arcade/at-the-crossroads/J7PQDxpWqhx7HrvBZ
 
 function metroCard(lastNumberOfDays) {
-  const months = [
-    /*Jan*/ 31,
-    /*Feb*/ 28,
-    /*Mar*/ 31,
-    /*Apr*/ 30,
-    /*May*/ 31,
-    /*Jun*/ 30,
-    /*Jul*/ 31,
-    /*Aug*/ 31,
-    /*Sep*/ 30,
-    /*Oct*/ 31,
-    /*Nov*/ 30,
-    /*Dec*/ 31
-  ];
-
-  switch(lastNumberOfDays){
-    case 28: return [31];
-    case 30: return [31];
-    case 31: return [28, 30, 31];
-  }
+  return lastNumberOfDays == 31 ? [28, 30, 31] : [31];
 }
