9c33ab4c3c20bba0003399d25e201adecac18301
Kyle Michael Sullivan <kylemichaelsullivan@gmail.com>
2019-06-22 23:13:01 -0400

    Remove superfluous code in 20 Mirror Bits

--- theCore/03 Corner of 0s and 1s/20-mirrorBits.js
+++ theCore/03 Corner of 0s and 1s/20-mirrorBits.js
@@ -1,7 +1,7 @@
 // https://app.codesignal.com/arcade/code-arcade/corner-of-0s-and-1s/e3zfPNTwTa9qTQzcX
 
 function mirrorBits(a) {
-  const arr = ('' + a.toString(2)).split('');
+  const arr = a.toString(2).split('');
   let z = '';
 
   for(let i = arr.length - 1; i >= 0; i--){
